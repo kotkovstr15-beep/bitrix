ad40dad45dd70bf2876311688dd7b01f254bd518
kotkovstr15-beep <kotkov.str.15@gmail.com>
2026-03-26 15:04:45 +0500

    Добавить установщик модуля и инструкции по установке

---
+++ bitrix/modules/vendor.sitewizard/install/version.php
@@ -0,0 +1,5 @@
+<?php
+$arModuleVersion = [
+    'VERSION' => '1.0.1',
+    'VERSION_DATE' => '2026-03-26 12:00:00',
+];
---
+++ bitrix/modules/vendor.sitewizard/lang/ru/install/index.php
@@ -0,0 +1,3 @@
+<?php
+$MESS['VENDOR_SITEWIZARD_MODULE_NAME'] = 'Vendor: мастер разворачивания сайта';
+$MESS['VENDOR_SITEWIZARD_MODULE_DESC'] = 'Устанавливает wizard для быстрого создания структуры сайта, инфоблоков и демо-данных.';
