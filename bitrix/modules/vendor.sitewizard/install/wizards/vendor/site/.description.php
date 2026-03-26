d65938413b3ff5131ae9b00a97fe33495e3bf558
kotkovstr15-beep <kotkov.str.15@gmail.com>
2026-03-26 16:04:14 +0500

    Сделать регистрацию шагов совместимой с версиями мастера

--- bitrix/modules/vendor.sitewizard/install/wizards/vendor/site/.description.php
+++ bitrix/modules/vendor.sitewizard/install/wizards/vendor/site/.description.php
@@ -6,8 +6,17 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 $arWizardDescription = [
     'NAME' => 'Мастер разворачивания сайта (Vendor)',
     'DESCRIPTION' => 'Автоматическое создание структуры сайта, инфоблоков, демо-данных и мультирегиональности.',
-    'VERSION' => '1.0.0',
+    'VERSION' => '1.0.1',
     'START_TYPE' => 'WINDOW',
+    'WIZARD_TYPE' => 'INSTALL',
+    'STEPS' => [
+        'Vendor\SiteWizard\Step\WelcomeStep',
+        'Vendor\SiteWizard\Step\EnvironmentCheckStep',
+        'Vendor\SiteWizard\Step\StructureChoiceStep',
+        'Vendor\SiteWizard\Step\RegionalSettingsStep',
+        'Vendor\SiteWizard\Step\InfrastructureBuildStep',
+        'Vendor\SiteWizard\Step\FinishStep',
+    ],
     'TEMPLATES' => [
         '.default' => [
             'SCRIPT' => '',
--- bitrix/modules/vendor.sitewizard/install/wizards/vendor/site/wizard.php
+++ bitrix/modules/vendor.sitewizard/install/wizards/vendor/site/wizard.php
@@ -6,12 +6,17 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 CModule::IncludeModule('main');
 require_once __DIR__ . '/scripts/init.php';
 
-/** @var CWizard $wizard */
-$wizard = $this;
+// Совместимость с разными версиями ядра:
+// - если доступно динамическое добавление шагов через AddStep, регистрируем шаги здесь;
+// - если нет (мастеры сайта в некоторых сборках), шаги берутся из .description.php (ключ STEPS).
+if (method_exists($this, 'AddStep')) {
+    /** @var CWizardBase $wizard */
+    $wizard = $this;
 
-$wizard->AddStep(new \Vendor\SiteWizard\Step\WelcomeStep());
-$wizard->AddStep(new \Vendor\SiteWizard\Step\EnvironmentCheckStep());
-$wizard->AddStep(new \Vendor\SiteWizard\Step\StructureChoiceStep());
-$wizard->AddStep(new \Vendor\SiteWizard\Step\RegionalSettingsStep());
-$wizard->AddStep(new \Vendor\SiteWizard\Step\InfrastructureBuildStep());
-$wizard->AddStep(new \Vendor\SiteWizard\Step\FinishStep());
+    $wizard->AddStep(new \Vendor\SiteWizard\Step\WelcomeStep());
+    $wizard->AddStep(new \Vendor\SiteWizard\Step\EnvironmentCheckStep());
+    $wizard->AddStep(new \Vendor\SiteWizard\Step\StructureChoiceStep());
+    $wizard->AddStep(new \Vendor\SiteWizard\Step\RegionalSettingsStep());
+    $wizard->AddStep(new \Vendor\SiteWizard\Step\InfrastructureBuildStep());
+    $wizard->AddStep(new \Vendor\SiteWizard\Step\FinishStep());
+}
